7707e5e8c2d404442bab8a8f81f03c4fa4016eea
RAFAEL DE OLIVEIRA ROCHA <frota@tropicalinspire.pt>
2026-01-10 18:34:21 +0000

    Fix: Enable global scrolling in App.tsx for mobile responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -466,7 +466,7 @@ function App() {
           )
           }
 
-          <div className="flex-1 overflow-hidden relative w-full flex flex-col min-h-0">
+          <div className="flex-1 overflow-y-auto custom-scrollbar relative w-full flex flex-col min-h-0">
             {renderContent()}
           </div>
 
